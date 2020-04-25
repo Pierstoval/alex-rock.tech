6d0a9cd37bf3fc14fc7bcdf669f91334732f581b
Alex Rock Ancelet <pierstoval@gmail.com>
2020-04-25 08:59:33 +0200

    Reorder deploy scripts

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -4,11 +4,11 @@ DIR=$(dirname "$0")
 
 cd "${DIR}"/..
 
+npm install
+npm run build
+
 composer install --no-interaction --no-progress --classmap-authoritative --no-dev --no-scripts
 php bin/console cache:clear --no-warmup
 sudo /etc/init.d/php7.4-fpm restart
 php bin/console cache:warmup
 php bin/console assets:install --symlink --relative
-
-npm install
-npm run build

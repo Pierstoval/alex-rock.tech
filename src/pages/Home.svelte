d3d4554be101d21c9b5e1748f0b446c99cc9e9c9
Alex Rock Ancelet <pierstoval@gmail.com>
2020-07-07 15:41:19 +0200

    -

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,11 +3,53 @@
     import Skills from "./Skills.svelte";
 </script>
 
+<style>
+    header.masthead {
+        position: relative;
+        overflow: hidden;
+        background: transparent url('/img/True_and_imagine_stars.png') repeat;
+    }
+    header.masthead .container {
+        position: relative;
+        z-index: 1;
+    }
+
+    #stars {
+        position: absolute;
+        z-index: 0;
+        background: transparent url('/img/True_and_imagine_stars.png') repeat;
+        width: 1920px;
+        height: 1965px;
+        top: 50%;
+        left: 50%;
+        margin-top: -982px;
+        margin-left: -960px;
+        -webkit-animation: spin-bg 360s linear infinite;
+        -moz-animation: spin-bg 360s linear infinite;
+        animation: spin-bg 360s linear infinite;
+    }
+    #moon {
+        background: transparent url('/img/pierstoval_moon_by_lawlie.png') no-repeat top center;
+        -webkit-background-size: 160px;
+        background-size: 160px;
+        width: 160px;
+        height: 160px;
+        margin: 50px auto 50px auto;
+        -webkit-animation: spin 270s linear infinite;
+        -moz-animation: spin 270s linear infinite;
+        animation: spin 270s linear infinite;
+    }
+
+    @-webkit-keyframes spin-bg { 100% { -webkit-transform: rotate(-360deg); } } @-moz-keyframes spin-bg { 100% { -moz-transform: rotate(-360deg); } } @keyframes spin-bg { 100% { -webkit-transform: rotate(-360deg); transform:rotate(-360deg); } }
+    @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } } @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } } @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
+</style>
+
 <svelte:head>
     <title>{$_('master_head.title')}</title>
 </svelte:head>
 
-<header class="masthead bg-primary text-white text-center pb-0">
+<header class="masthead text-white text-center pb-0">
+    <div id="stars"></div>
     <div class="container d-flex align-items-center flex-column">
 
         <img class="masthead-avatar mb-5" src="/img/avatar.jpg" alt="">
@@ -26,6 +68,8 @@
             {$_('master_head.title')}
         </p>
     </div>
+
+    <div id="moon"></div>
 </header>
 
 <Skills/>
